172eb278b5a2ed1a095299f6e4a2db9042e334e2
Anna Shevchuk <annashevchuk2511@gmail.com>
2018-10-12 20:46:50 +0300

    change adaptiv version

--- app/js/main.js
+++ app/js/main.js
@@ -11,22 +11,7 @@ $(document).ready(function(){
     integer: 3,
   	responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        arrows: true,
-      }
-    },
-    {
-      breakpoint: 995,
-      settings: {
-        slidesToShow: 4,
-        arrows: true,
-      }
-    },
-    {
-      breakpoint: 600,
+      breakpoint: 1025,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 2,
@@ -34,9 +19,16 @@ $(document).ready(function(){
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 815,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
       }
