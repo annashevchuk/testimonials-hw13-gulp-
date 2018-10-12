d35a7c88e83ce4761cc76660bd13565cec011b76
Anna Shevchuk <annashevchuk2511@gmail.com>
2018-10-12 20:28:21 +0300

    change in slider and adaptive version

--- app/js/main.js
+++ app/js/main.js
@@ -11,59 +11,37 @@ $(document).ready(function(){
     integer: 3,
   	responsive: [
     {
-      breakpoint: 992,
+      breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
-        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: true,
+      }
+    },
+    {
+      breakpoint: 995,
+      settings: {
+        slidesToShow: 4,
+        arrows: true,
       }
     },
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 2,
-        arrows: false,
+        arrows: true,
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
       }
     }
   ]
   });
-
-
-  
-
-
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
 
 });
